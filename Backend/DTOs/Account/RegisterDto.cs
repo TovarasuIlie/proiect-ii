56f3453ffb800fe4532ead9e50eb6ab53d8936ff
dtoader69 <dtoader69@gmail.com>
2024-03-15 12:24:39 +0200

    mail confirmatio+password reset

--- Backend/DTOs/Account/RegisterDto.cs
+++ Backend/DTOs/Account/RegisterDto.cs
@@ -5,13 +5,13 @@ namespace Backend.DTOs.Account
     public class RegisterDto
     {
         [Required]
-        [StringLength(30,MinimumLength =3,ErrorMessage ="Name must be at least {2} and maximum {1} characters")]
+        [StringLength(30,MinimumLength =3,ErrorMessage ="Numele trebuie sa contina minimum {2} si maximum {1} caractere")]
         public string FullName { get; set; }
         [Required]
-        [RegularExpression("^\\w+@[a-zA-Z_]+?\\.[a-zA-Z]{2,3}$", ErrorMessage ="Invalid Email Address")]
+        [RegularExpression("^\\w+@[a-zA-Z_]+?\\.[a-zA-Z]{2,3}$", ErrorMessage ="Adresa de email invalida")]
         public string Email { get; set; }
         [Required]
-        [StringLength(30,MinimumLength =6,ErrorMessage ="Password must be at least {2} and maximum {1} characters")]
+        [StringLength(30,MinimumLength =6,ErrorMessage ="Parola trebuie sa contina minimum {2} si maximum {1} caractere")]
         public string Password { get; set; }
         
         public string Phone { get; set; }
