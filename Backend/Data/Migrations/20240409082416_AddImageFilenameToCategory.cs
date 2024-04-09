47d7dbb878d7a2e73f8e0f383e38651bafab7aae
dtoader69 <dtoader69@gmail.com>
2024-04-09 11:25:44 +0300

    migration for image

---
+++ Backend/Data/Migrations/20240409082416_AddImageFilenameToCategory.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddImageFilenameToCategory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageFilename",
+                table: "Categories",
+                type: "nvarchar(max)",
+                nullable: true,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageFilename",
+                table: "Categories");
+        }
+    }
+}
