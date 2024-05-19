6ece93acf3a8412163f7207b51af9d0175285900
dtoader69 <dtoader69@gmail.com>
2024-05-19 12:34:33 +0300

    order controller+model

---
+++ Backend/Models/Shop/Order.cs
@@ -0,0 +1,10 @@
+﻿namespace Backend.Models.Shop
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public bool IsConfirmed { get; set; }
+        public ICollection<OrderDetails> OrderDetails { get; set; }
+    }
+}
